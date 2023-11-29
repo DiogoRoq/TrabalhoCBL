f8aa02e79533b7e223048ef518d74f59a9837e98
LuccasPL <117682893+LuccasPL@users.noreply.github.com>
2023-11-29 19:57:45 +0000

    Create script.js

---
+++ Site atualizado/script.js
@@ -0,0 +1,22 @@
+// Exemplo de função para manipular cliques no menu
+document.querySelectorAll('nav ul li a').forEach(item => {
+    item.addEventListener('click', function(e) {
+        e.preventDefault();
+        alert('Link clicado: ' + this.textContent);
+    });
+});
+
+// Efeito de hover para linhas da tabela
+document.querySelectorAll('#news-list table tr').forEach(row => {
+    row.addEventListener('mouseover', () => {
+        row.style.backgroundColor = '#cccccc';
+    });
+    row.addEventListener('mouseout', () => {
+        row.style.backgroundColor = '';
+    });
+});
+
+// Abrir links em nova aba
+document.querySelectorAll('#news-list table a').forEach(link => {
+    link.setAttribute('target', '_blank');
+});

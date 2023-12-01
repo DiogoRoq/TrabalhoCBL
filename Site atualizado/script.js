07d1bf047bf99a1a51f79918b7334467e9422d4d
LuccasPL <117682893+LuccasPL@users.noreply.github.com>
2023-12-01 20:02:36 +0000

    Update script.js

--- Site atualizado/script.js
+++ Site atualizado/script.js
@@ -21,3 +21,39 @@ document.querySelectorAll('#featured-news table a').forEach(link => {
     link.setAttribute('target', '_blank');
 }); // nova aba quando clica no link 
 
+// Espera por todo o conteúdo da página ser carregado
+document.addEventListener("DOMContentLoaded", () => {
+    // Aplica a animação de entrada
+    document.body.classList.add("fade-in");
+  
+    // Intercepta todos os cliques em links
+    document.querySelectorAll("a").forEach(link => {
+      link.addEventListener("click", e => {
+        e.preventDefault(); // Previne o comportamento padrão do link
+        const href = link.getAttribute("href");
+  
+        // Aplica a animação de saída
+        document.body.classList.add("fade-out");
+  
+        // Aguarda o fim da animação
+        setTimeout(() => {
+          window.location.href = href; // Redireciona para o link clicado
+        }, 500); // O tempo aqui deve corresponder à duração da animação fade-out
+      });
+    });
+  });
+
+  document.getElementById('searchForm').addEventListener('submit', function(e) {
+    e.preventDefault(); // Impede o recarregamento da página
+    const searchTerm = document.getElementById('searchBox').value.toLowerCase();
+
+    // Percorre todos os títulos
+    document.querySelectorAll('#featured-news table tr td:first-child').forEach(title => {
+        const titleText = title.textContent.toLowerCase();
+        if (titleText.indexOf(searchTerm) > -1) {
+            title.parentElement.style.display = 'table-row';
+        } else {
+            title.parentElement.style.display = 'none';
+        }
+    });
+});
